df60e32d0ae63f0cd53abf08faac470fa02442b7
Harb3rt0 <luisalbertohdzv09@gmail.com>
2025-03-10 20:34:24 -0600

    Organización de carpetas del proyecto

---
+++ server/main.js
@@ -0,0 +1,9 @@
+const port = 8080;
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('./public' + '/'));
+app.listen(port, () => {
+    console.log("Servidor corriendo en: http://localhost:" + port);
+})
