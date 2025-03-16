f739bcbfb92b51353b3bf645ec25854f1a6f4461
José Luis Espinoza Sánchez <joseluisessa@gmail.com>
2025-03-15 20:57:47 -0600

    Limpieza para agregar todo el contenido de la rama 'Alberto'

--- server/main.js
+++
@@ -1,9 +0,0 @@
-const port = 8080;
-const express = require('express');
-
-const app = express();
-
-app.use(express.static('./public' + '/'));
-app.listen(port, () => {
-    console.log("Servidor corriendo en: http://localhost:" + port);
-})
